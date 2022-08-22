c71cbb7c0362056b8b6949194deab72159209f2e
Imane BOUROUYNE (MA/FR) <110111059+Curiosity10001@users.noreply.github.com>
2022-08-22 11:59:37 +0200

    raycast first try
    raycast first try

--- Assets/RepulsionOnColision.cs
+++ Assets/RepulsionOnColision.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RepulsionOnColision : MonoBehaviour
 {
+    public ParticleSystemForceField _mBox;
+    public Vector2 _info;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,6 @@ public class RepulsionOnColision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
        
-       
+      
     }
 }
